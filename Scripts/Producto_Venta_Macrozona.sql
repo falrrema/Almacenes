52a74cb0a63317e324a9d3016656ef3a84f307f6
Fabian Reyes Madrid <fabian.reyes@bci.cl>
2022-01-31 14:55:00 -0300

    Scripts productos y categorias listos

--- Scripts/Producto_Venta_Macrozona.sql
+++ Scripts/Producto_Venta_Macrozona.sql
@@ -1,6 +1,6 @@
 /***************************************************************************************
 --****************************************************************************************
---** DSCRPCN: Porcentaje de almacenes que venden por categoría por macrozona            **
+--** DSCRPCN: Porcentaje de almacenes que venden por producto  por macrozona            **
 --**                                                                                    **
 --** AUTOR  : Fabian Reyes                                                              **
 --** FECHA  : 2022-01-28                                                                **
@@ -89,66 +89,64 @@ select a.boleta_id
 , e.almacen_id 
 , a.cantidad 
 , a.total
-, a.nombre_producto 
+, c.nombre AS producto
 , b.producto_id
 , c.categoria_producto_id 
-, d.nombre AS categoria
 , f.macrozona
 FROM almacenes_digitales.detalle_compra a
 LEFT JOIN almacenes_digitales.producto_almacen b ON a.codigo_producto_almacen = b.producto_almacen_id 
 LEFT JOIN almacenes_digitales.producto c ON b.producto_id = c.producto_id 
-LEFT JOIN almacenes_digitales.categoria_producto d ON c.categoria_producto_id = d.categoria_producto_id 
 LEFT JOIN almacenes_digitales.boleta e ON a.boleta_id = e.boleta_id 
 INNER JOIN georeferencia_almacen f ON e.almacen_id = f.almacen_id
-WHERE d.nombre IS NOT NULL -- que tenga categoria
+WHERE c.nombre  IS NOT NULL -- que tenga producto
 ),
 
-cat_macrozona_agg AS (
-SELECT a.categoria
+prod_macrozona_agg AS (
+SELECT a.producto
 , a.macrozona
-, count(DISTINCT a.almacen_id) AS n_almacenes_categoria
+, count(DISTINCT a.almacen_id) AS n_almacenes_producto
 , b.n_almacenes AS n_almacenes_macrozona
-, round(count(DISTINCT a.almacen_id)/b.n_almacenes, 3) AS porc_categoria
+, round(count(DISTINCT a.almacen_id)/b.n_almacenes, 3) AS porc_producto
 FROM prod_cat_macrozona a
 LEFT JOIN (
     select macrozona
     , count(*) AS n_almacenes
     from georeferencia_almacen
     GROUP BY macrozona) b ON a.macrozona = b.macrozona
-GROUP BY categoria, macrozona
+GROUP BY producto, macrozona
 ),
 
 tablon_inferencia_almacenes AS (
-SELECT a.categoria
+SELECT a.producto
 , a.macrozona
-, a.n_almacenes_categoria
+, a.n_almacenes_producto
 , a.n_almacenes_macrozona
-, a.porc_categoria
+, a.porc_producto
 , b.n_almacenes AS n_almacenes_totales
-, round(SQRT((a.porc_categoria * (1 - a.porc_categoria))/a.n_almacenes_macrozona), 4) AS error_std
-, round(a.porc_categoria - 1.96*SQRT((a.porc_categoria * (1 - a.porc_categoria))/a.n_almacenes_macrozona), 3) AS porc_inf
-, round(a.porc_categoria + 1.96*SQRT((a.porc_categoria * (1 - a.porc_categoria))/a.n_almacenes_macrozona), 3) AS porc_sup
-, round(b.n_almacenes*(a.porc_categoria + 1.96*SQRT((a.porc_categoria * (1 - a.porc_categoria))/a.n_almacenes_macrozona)), 0) AS n_alm_sup
-, round(b.n_almacenes*(a.porc_categoria - 1.96*SQRT((a.porc_categoria * (1 - a.porc_categoria))/a.n_almacenes_macrozona)), 0) AS n_alm_inf
-FROM cat_macrozona_agg a
+, round(SQRT((a.porc_producto * (1 - a.porc_producto))/a.n_almacenes_macrozona), 4) AS error_std
+, round(a.porc_producto - 1.96*SQRT((a.porc_producto * (1 - a.porc_producto))/a.n_almacenes_macrozona), 3) AS porc_inf
+, round(a.porc_producto + 1.96*SQRT((a.porc_producto * (1 - a.porc_producto))/a.n_almacenes_macrozona), 3) AS porc_sup
+, round(b.n_almacenes*(a.porc_producto + 1.96*SQRT((a.porc_producto * (1 - a.porc_producto))/a.n_almacenes_macrozona)), 0) AS n_alm_sup
+, round(b.n_almacenes*(a.porc_producto - 1.96*SQRT((a.porc_producto * (1 - a.porc_producto))/a.n_almacenes_macrozona)), 0) AS n_alm_inf
+FROM prod_macrozona_agg a
 LEFT JOIN georeferencia_almacenes_nacionales b ON a.macrozona = b.macrozona
 ) 
 
-SELECT categoria
+SELECT producto
 , macrozona
-, n_almacenes_categoria
+, n_almacenes_producto
 , n_almacenes_macrozona
-, porc_categoria
+, porc_producto
 , n_almacenes_totales
 , error_std 
 , CASE WHEN porc_inf < 0 THEN 0 ELSE porc_inf END AS porc_inf
 , porc_sup
 , CASE WHEN n_alm_inf < 0 THEN 0 ELSE n_alm_inf END AS n_alm_inf
-, round(porc_categoria*n_almacenes_totales) AS n_alm_esperado
+, round(porc_producto*n_almacenes_totales) AS n_alm_esperado
 , n_alm_sup
-, porc_categoria*n_almacenes_macrozona AS cond_1
-, (1 - porc_categoria)*n_almacenes_macrozona AS cond_2
-, CASE WHEN porc_categoria*n_almacenes_macrozona >= 10 AND (1-porc_categoria)*n_almacenes_macrozona >= 10 THEN 'Confiable'
+, porc_producto*n_almacenes_macrozona AS cond_1
+, (1 - porc_producto)*n_almacenes_macrozona AS cond_2
+, CASE WHEN porc_producto*n_almacenes_macrozona >= 10 AND (1-porc_producto)*n_almacenes_macrozona >= 10 THEN 'Confiable'
     ELSE 'No Confiable' END AS confianza_estimacion
 FROM tablon_inferencia_almacenes
 
